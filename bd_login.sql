6f0664ba5364d69e61738fbaef32f265c46d53e4
Pierr Daniel Chino Lurita <39500716+codepcl@users.noreply.github.com>
2018-05-21 16:22:05 -0500

    Add files via upload

---
+++ bd_login.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-05-2018 a las 23:21:20
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_login`
+--
+CREATE DATABASE IF NOT EXISTS `bd_login` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bd_login`;
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_agregarUsuario` (IN `var_dni` CHAR(8), IN `var_nombre` VARCHAR(255), IN `var_apellido` VARCHAR(255), IN `var_direccion` VARCHAR(255), IN `var_telefono` VARCHAR(255), IN `var_usuario` VARCHAR(255), IN `var_password` VARCHAR(255), IN `var_tipoUsuario` INT, IN `var_Estado` INT)  BEGIN
+	INSERT INTO usuario (
+		usuario.DNI,
+		usuario.Nombre,
+		usuario.Apellido,
+		usuario.Direccion,
+		usuario.Telefono,
+		usuario.Usuario,
+		usuario.`Password`,
+		usuario.Tipo_Usuario,
+		usuario.Estado
+	)
+VALUES
+	(
+		var_dni,
+		var_nombre,
+		var_apellido,
+		var_direccion,
+		var_telefono,
+		var_usuario,
+		var_password,
+		var_tipoUsuario,
+		var_Estado
+	);
+
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `idEstado` int(11) NOT NULL,
+  `Estado` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`idEstado`, `Estado`) VALUES
+(1, 'Activo'),
+(2, 'Inactivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `idTipo_Usuario` int(11) NOT NULL,
+  `Tipo_Usuario` varchar(255) DEFAULT NULL,
+  `Tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`idTipo_Usuario`, `Tipo_Usuario`, `Tipo`) VALUES
+(1, 'admin', 1),
+(2, 'usuario', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `DNI` char(8) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Apellido` varchar(255) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Telefono` varchar(255) DEFAULT NULL,
+  `Usuario` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Estado` int(11) NOT NULL,
+  `Tipo_Usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `DNI`, `Nombre`, `Apellido`, `Direccion`, `Telefono`, `Usuario`, `Password`, `Estado`, `Tipo_Usuario`) VALUES
+(1, '70417651', 'Ray', 'Siu', NULL, '324324', 'ray123', '123456', 1, 1),
+(18, '12345678', 'Juan', 'Perez', 'begonias', '94654684', 'jperez', '123456', 2, 2),
+(19, '12345688', 'Luisa', 'Ruiz', 'miraflores', '444444', 'lruiz', '147', 1, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`idTipo_Usuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`,`Estado`,`Tipo_Usuario`),
+  ADD UNIQUE KEY `Usuario_UNIQUE` (`Usuario`),
+  ADD KEY `fk_Usuario_Estado_idx` (`Estado`),
+  ADD KEY `fk_Usuario_Tipo_Usuario1_idx` (`Tipo_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `idTipo_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_Usuario_Estado` FOREIGN KEY (`Estado`) REFERENCES `estado` (`idEstado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Usuario_Tipo_Usuario1` FOREIGN KEY (`Tipo_Usuario`) REFERENCES `tipo_usuario` (`idTipo_Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
